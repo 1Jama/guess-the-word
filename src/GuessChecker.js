f8893e016881f014cf10ac1228aff22d3f5a1773
1Jama <Jamaf@my.yorku.ca>
2022-06-15 06:19:53 -0400

    Created comparison function

---
+++ src/GuessChecker.js
@@ -0,0 +1,8 @@
+import React, { Component, useState } from 'react';
+import App from './App';
+
+const GuessChecker = (word, guess) => {
+  return console.log(word === guess);
+};
+
+export default GuessChecker;
