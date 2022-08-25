c87190ce7a7c94d916af15b32ea60d2af24c1989
1Jama <Jamaf@my.yorku.ca>
2022-08-25 08:42:44 -0400

    server test

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config({ path: './config.env' });
-export const port = 3001;
+const port = 3001;
 app.use(cors());
 app.use(express.json());
 app.use(require('./words'));
