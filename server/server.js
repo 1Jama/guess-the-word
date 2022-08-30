59ce9956856e0ca4949aef5b02184d22c6bd3910
1Jama <Jamaf@my.yorku.ca>
2022-08-30 08:18:27 -0400

    added procfile

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.disable('etag');
 
 const path = require('path');
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
