8da21ddf1814e68a4a0153ed374310addc04d5a8
1Jama <Jamaf@my.yorku.ca>
2023-08-16 02:56:21 -0400

    updated fly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import AnimatedRoutes from './AnimatedRoutes';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import React, { Component, useState } from 'react';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
